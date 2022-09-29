2376121dfa79c497d00f8eaa1c7690319328c726
mokshith <83539464+mokshith1618@users.noreply.github.com>
2022-09-29 00:08:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
+const http = require("http");
+const fs = require("fs");
+fs.readFile("home.html", (err, home) => {
+  console.log(home.toString());
+});
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  http
+    .createServer((request, response) => {
+      response.writeHeader(200, { "Content-Type": "text/html" });
+      response.write(home);
+      response.end();
+    })
+    .listen(5000);
+});
+const http = require("http");
+const fs = require("fs");
 
+let homeContent = "";
+let projectContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(5000);
